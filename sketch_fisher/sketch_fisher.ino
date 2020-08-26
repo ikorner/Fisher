739dc684f06e3008b7604482d290a039e9eb41b2
ikorner <fed.alexeykor@gmail.com>
2020-08-26 18:51:11 +0300

    change sketch

--- sketch_fisher/sketch_fisher.ino
+++ sketch_fisher/sketch_fisher.ino
@@ -1,10 +1,14 @@
 #include <Keyboard.h>
 #define LED_PIN SS
 
-char inChar;
-char F1 = KEY_F1;
+// задание необходимых переменных
+char inChar; 
+char F1 = KEY_F1; 
 char F2 = KEY_F2;
 char F3 = KEY_F3;
+char F4 = KEY_F4;
+char F5 = KEY_F5;
+
 
 void setup() {
   pinMode(LED_PIN, OUTPUT); // Инициализация светодиода
@@ -16,7 +20,7 @@ void loop() {
   if (Serial.available() > 0)
   {
     inChar = Serial.read();
-    if (inChar=='e') // e - Enable - включить
+    if (inChar=='e') // e - Enable - включить светодиод. Начало "рыбалки" - нажатие F1
     {
       digitalWrite(LED_PIN,HIGH);
       Keyboard.press(F1);
@@ -26,7 +30,7 @@ void loop() {
     }
   }
   
-    else if (inChar=='d') // d - Disable - выключить
+    else if (inChar=='d') // d - Disable - выключить светодиод. "Подтягивание" рыбы - нажатие F2
     {
       digitalWrite(LED_PIN,LOW);
        Keyboard.press(F2);
@@ -35,7 +39,7 @@ void loop() {
       inChar = '1';
     }
   
-    else if (inChar=='b')  // b - Blink - выключить режим мигания
+    else if (inChar=='b')  // b - Blink - выключить режим мигания. "Подсекание" рыбы - Нажатие F3
     {
       
       digitalWrite(LED_PIN,HIGH);
@@ -47,5 +51,16 @@ void loop() {
       Keyboard.releaseAll();
       inChar = '1';
     }
-    
+
+    else if (inChar=='1') // После каждого действия отправляем нажатие F4 и F5, где настроено "секретное действие"
+    {
+      Keyboard.press(F4);
+      delay(300);
+      Keyboard.releaseAll();
+      Keyboard.press(F5);
+      delay(300);
+      Keyboard.releaseAll();
+      inChar = '2';
+    }
+    }
 }
